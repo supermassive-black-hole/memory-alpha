de02bdacda9c3b12ac642ff13b619c278a897077
Stanislas Nanchen <stan@hood.ch>
2017-10-30 14:21:50 +0100

    init script is not root

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM postgres:10
 
-ADD archivecp /opt/archivecp
+ADD archivecp /opt/memory-alpha/archivecp
 ADD init-backup.sh /docker-entrypoint-initdb.d/init-backup.sh
 
-RUN apt-get update \
+RUN chown -R postgres /opt/memory-alpha \
+ && chgrp -R postgres /opt/memory-alpha \
+ && apt-get update \
  && apt-get install -y python3-pip \
  && pip3 install awscli \
  && apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
--- init-backup.sh
+++ init-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sed -i -e "s/^#archive_mode = off/archive_mode = on/" /var/lib/postgresql/data/postgresql.conf
-sed -i -e "s/^#archive_command = ''/archive_command = '\/opt\/archivecp %p %f'/" /var/lib/postgresql/data/postgresql.conf
-sed -i -e "s/MEMORY_ALPHA_S3_BUCKET=/MEMORY_ALPHA_S3_BUCKET=${MEMORY_ALPHA_S3_BUCKET}/" /opt/archivecp
+sed -i -e "s/^#archive_command = ''/archive_command = '\/opt\/memory-alpha\/archivecp %p %f'/" /var/lib/postgresql/data/postgresql.conf
+sed -i -e "s/MEMORY_ALPHA_S3_BUCKET=/MEMORY_ALPHA_S3_BUCKET=${MEMORY_ALPHA_S3_BUCKET}/" /opt/memory-alpha/archivecp
