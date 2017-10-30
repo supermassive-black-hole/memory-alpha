a7affb405c6c5cf8e2f07e1290c7bbcf3f48b9f7
Stanislas Nanchen <stan@hood.ch>
2017-10-30 13:59:38 +0100

    fix init script

--- init-backup.sh
+++ init-backup.sh
@@ -2,4 +2,4 @@
 
 sed -i -e "s/^#archive_mode = off/archive_mode = on/" /var/lib/postgresql/data/postgresql.conf
 sed -i -e "s/^#archive_command = ''/archive_command = '\/opt\/archivecp %p %f'/" /var/lib/postgresql/data/postgresql.conf
-sed -i -e "s/MEMORY_ALPHA_S3_BUCKET=/MEMORY_ALPHA_S3_BUCKET=${MEMORY_ALPHA_S3_BUCKET}/" /srv/archivecp
+sed -i -e "s/MEMORY_ALPHA_S3_BUCKET=/MEMORY_ALPHA_S3_BUCKET=${MEMORY_ALPHA_S3_BUCKET}/" /opt/archivecp
