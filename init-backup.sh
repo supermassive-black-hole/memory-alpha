b7f9e9608159cbb349de898b49d735350cb6b563
Stanislas Nanchen <stan@hood.ch>
2017-10-31 13:45:13 +0100

    finish backupscript.

--- init-backup.sh
+++ init-backup.sh
@@ -2,5 +2,6 @@
 
 sed -i -e "s/^#archive_mode = off/archive_mode = on/" /var/lib/postgresql/data/postgresql.conf
 sed -i -e "s/^#archive_command = ''/archive_command = '\/opt\/memory-alpha\/archivecp %p %f'/" /var/lib/postgresql/data/postgresql.conf
-sed -i -e "s/^#archive_timeout = 0/archive_timeout = 3600/" /var/lib/postgresql/data/postgresql.conf
+sed -i -e "s/^#archive_timeout = 0/archive_timeout = 300/" /var/lib/postgresql/data/postgresql.conf
 sed -i -e "s/MEMORY_ALPHA_S3_BUCKET=/MEMORY_ALPHA_S3_BUCKET=${MEMORY_ALPHA_S3_BUCKET}/" /opt/memory-alpha/archivecp
+sed -i -e "s/MEMORY_ALPHA_S3_BUCKET=/MEMORY_ALPHA_S3_BUCKET=${MEMORY_ALPHA_S3_BUCKET}/" /opt/memory-alpha/basebackup
